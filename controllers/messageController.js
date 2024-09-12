5320fbd3bbe97e8fd7c898cddce4a610bf4ce5cb
Raul Duarte <raultorresoliveira@gmail.com>
2024-09-12 11:01:20 -0300

    Melhoria no sistema de tratamento de exceções

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,48 +1,44 @@
 import Message from "../models/Message.js";
 import { Op } from "sequelize";
 
-const possibleStatus = ["ENVIADO", "RECEBIDO", "ERRO DE ENVIO"];
+const HTTP_STATUS_ACCEPTED = 202;
+const HTTP_STATUS_BAD_REQUEST = 400;
+const HTTP_STATUS_NOT_FOUND = 404;
+const HTTP_STATUS_INTERNAL_SERVER = 500;
+const POSSIBLE_STATUS = ["ENVIADO", "RECEBIDO", "ERRO DE ENVIO"];
 
 export const updateMessageStatus = async (req, res) => {
-    const { id, status } = req.params;
-    let statusToUpper = status.toUpperCase().replace(/-/g, ' ');
-
-    if (!/^\d+$/.test(id)) {
-        return res.status(400).json({ error: "O id da mensagem deve ser um número" });
-    }
-
-    if (!possibleStatus.includes(statusToUpper)) {
-        return res.status(400).json({ error: "Status inválido" });
-    }
-
     try {
+	    const [id, status] = processRequest(req);
+
         const message = await Message.findByPk(id);
+
         if (!message) {
-            return res.status(404).json({ error: "Mensagem não encontrada" });
+            
         }
 
-        message.status = statusToUpper;
+        message.status = status;
+
         await message.save();
-        return res.status(200).json({ message: "Mensagem atualizada com sucesso" });
+
+        return res.status(HTTP_STATUS_ACCEPTED).json({ message: "Mensagem atualizada com sucesso" });
     } catch (error) {
-        return res.status(500).json({ error: error.message });
+        console.log(error);
+        return res.status(error.code ?? HTTP_STATUS_INTERNAL_SERVER).json({ error: error.message });
     }
 };
 
 export const getMessagesByStatus = async (req, res) => {
     const { status } = req.params;
-    const now = new Date();
-    const last24Hours = new Date(now.getTime() - 24 * 60 * 60 * 1000);
-    let statusToUpper = status.toUpperCase().replace(/-/g, ' ');
-
-    if (!possibleStatus.includes(statusToUpper)) {
-        return res.status(400).json({ error: "Status inválido" });
-    }
+    const last24Hours = getLast24Hours()
 
     try {
+
+		processStatus(status);
+
         const items = await Message.findAll({
             where: {
-                status: statusToUpper,
+                status: status,
                 updatedAt: {
                     [Op.gte]: last24Hours,
                 },
@@ -50,11 +46,45 @@ export const getMessagesByStatus = async (req, res) => {
         });
 
         if (items.length === 0) {
-            return res.status(404).json({ error: "Nenhuma mensagem encontrada" });
+            throw new Error("Nenhuma mensagem encontrada.",  {statusCode: HTTP_STATUS_NOT_FOUND});
         }
 
-        return res.status(200).json({ items });
+        return res.status(HTTP_STATUS_ACCEPTED).json({ items });
     } catch (error) {
-        return res.status(500).json({ error: error.message });
+        return res.status(error.statusCode ?? HTTP_STATUS_INTERNAL_SERVER).json({ error: error.message });
     }
 };
+
+const processRequest = (req) => {
+    const { id, status } = req.params;
+
+	processID(id);
+	processStatus(status);
+
+    return [id, status];
+}
+
+const processID = (id) => {
+    if (!/^\d+$/.test(id)) {
+        throwException("O ID da mensagem deve ser numérico.", HTTP_STATUS_BAD_REQUEST);
+    }
+}
+
+const processStatus = (status) => {
+    status = status.toUpperCase().replace(/-/g, ' ');
+
+    if (!POSSIBLE_STATUS.includes(status)) {
+        throwException("O status informado é inválido", HTTP_STATUS_BAD_REQUEST);
+    }
+}
+
+const getLast24Hours = _ => {
+    let now = new Date();
+    return new Date(now.getTime() - 24 * 60 * 60 * 1000);
+}
+
+const throwException = (message, statusCode) => {
+    const error = new Error(message);
+    error.code = statusCode;
+    throw error;
+ }
